68ed5cc7be7a05a64267f660970c2f5b118675f3
maqibin <maqi1520@163.com>
2021-12-08 13:12:36 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ export const title = '狂奔小马的博客';
 export const keywords =
   '狂奔滴小马, 个人博客, web 全栈, web前端, JavaScript, css, html, nodejs';
 export const description = '狂奔小马的博客,前端工程师,web 全栈开发者';
-export const email = 'mail1520@163.com';
+export const email = 'maqi1520@163.com';
 export const wechat = 'maqibin1990';
 export const github = 'https://github.com/maqi1520';
 export const copyright = '@copyright 2016-2021';
