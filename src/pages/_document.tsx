692a0a169df69610efe872768b629460bec95aea
马琪斌 <maqi1520@163.com>
2021-12-20 15:05:53 +0800

    feat: add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,23 @@ class MyDocument extends Document {
           <meta name="keywords" content={keywords} />
           <link rel="icon" href="/favicon.ico" />
           <script type="text/javascript" src="/theme.js"></script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body className="text-skin-base transition-colors bg-skin-base text-sm">
           <Main />
