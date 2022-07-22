dcd30a79876b692163c71fa1d498dec863af74a3
马琪斌 <maqi1520@163.com>
2022-07-22 11:05:21 +0800

    feat: 🎸 添加导航工具

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: '文章' },
   { href: '/tags', title: '标签' },
   { href: '/projects', title: '项目' },
+  { href: 'https://www.runjs.cool/', title: '工具' },
   { href: '/about', title: '关于' },
 ]
 
