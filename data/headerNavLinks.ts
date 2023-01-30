c4a4ae47d5564ec540c85cd137e619cc046cbc8f
马琪斌 <maqi1520@163.com>
2023-01-30 15:21:41 +0800

    feat: 🎸 add RSS

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/projects', title: '项目' },
   { href: 'https://www.runjs.cool/', title: '工具' },
   { href: '/about', title: '关于' },
+  { href: '/feed.xml', title: 'RSS' },
 ]
 
 export default headerNavLinks
