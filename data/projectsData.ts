1890d5a97f58f97ad6d8a7d36aa8605eccb07d9d
马琪斌 <maqi1520@163.com>
2023-07-10 16:50:53 +0800

    fix: 🐛 链接错误

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,7 @@ const projectsData = [
     title: 'MDX-Editor',
     description: `微信排版编辑器，使用MDX，可自定义组件、样式、生成二维码、代码 diff 高亮，可导出 markdown 和 PDF。`,
     imgSrc: '/static/images/mdx-editor.png',
-    href: 'https://edotor.runjs.cool/',
+    href: 'https://editor.runjs.cool/',
   },
   {
     title: '在线简历生成器',
